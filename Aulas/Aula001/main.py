1b89ad08169330cc13b218ec7d06a124ce389ac5
ramon.valeriano <ramon.valeriano@netrin.com.br>
2022-06-28 21:06:55 -0300

    [0.0.1]-[20220628]_6-finalizando-Aula001

--- Aulas/Aula001/main.py
+++ Aulas/Aula001/main.py
@@ -10,4 +10,4 @@ async def mensagem():
     return {'mensage': 'Tudo certo!'}
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
