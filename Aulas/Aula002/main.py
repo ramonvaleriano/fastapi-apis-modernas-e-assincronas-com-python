1a9e2e2c16df8630bf631b219576f74b4c46a8d9
ramon.valeriano <ramon.valeriano@netrin.com.br>
2022-06-29 12:09:36 -0300

    [0.0.1]-[20220629]_3-criando-primeiro-requisocao-post

--- Aulas/Aula002/main.py
+++ Aulas/Aula002/main.py
@@ -1,5 +1,9 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    HTTPException,
+    status
+)
 from models import CursosModel
 
 app = FastAPI()
@@ -18,17 +22,32 @@ cursos = {
 }
 
 
-@app.get('/cursos')
+@app.get('/cursos', status_code=status.HTTP_200_OK, tags=['Coletando todos os cursos'])
 async def get_cursos():
 
     return cursos
 
 
-@app.get('/cursos/{curso_id}')
+@app.get('/cursos/{curso_id}', status_code=status.HTTP_200_OK, tags=['Coletando um Curso especifico'])
 async def get_curso(curso_id: int):
-    curso = cursos[curso_id]
-    curso.update({'id': curso_id})
-    return curso
+    print(curso_id)
+    try:
+        curso = cursos[curso_id]
+        curso.update({'id': curso_id})
+        return curso
+
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado')
+
+
+@app.post('/cursos', status_code=status.HTTP_201_CREATED, tags=['Adicionando um Curso no banco de dados'])
+async def add_curso(curso: CursosModel):
+    if curso.id not in cursos:
+        cursos[curso.id] = curso
+        return curso
+
+    else:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Já existe esse ID no banco')
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True, debug=True)
