b25cc5a83e887c0f170490253e7aeaf4a6a79f62
ramon.valeriano <ramon.valeriano@netrin.com.br>
2022-06-30 10:11:08 -0300

    [0.0.1]-[20220630]_1-usando-depends

--- Aulas/Aula002/main.py
+++ Aulas/Aula002/main.py
@@ -1,5 +1,6 @@
 import uvicorn
-from typing import Optional
+from time import sleep
+from typing import Optional, Any
 from models import CursosModel
 from fastapi import (
     FastAPI,
@@ -8,8 +9,19 @@ from fastapi import (
     Response,
     Path,
     Query,
-    Header
+    Header,
+    Depends
 )
+
+
+def fake_db():
+    try:
+        print('Abrindo Conexão com o banco de dados')
+        sleep(2)
+    finally:
+        print('Fechando Conexão com o banco de dados')
+        sleep(1)
+
 
 app = FastAPI()
 
@@ -28,14 +40,14 @@ cursos = {
 
 
 @app.get('/cursos', status_code=status.HTTP_200_OK, tags=['Coletando todos os cursos'])
-async def get_cursos():
+async def get_cursos(db: Any = Depends(fake_db)):
 
     return cursos
 
 
 @app.get('/cursos/{curso_id}', status_code=status.HTTP_200_OK, tags=['Coletando um Curso especifico'])
 async def get_curso(curso_id: int = Path(default=None, title='ID do Curso', description='Deve ser entre 1 e 2',
-                                         gt=0, lt=3)):
+                                         gt=0, lt=3), db: Any = Depends(fake_db)):
     try:
         curso = cursos[curso_id]
         curso.update({'id': curso_id})
@@ -46,7 +58,7 @@ async def get_curso(curso_id: int = Path(default=None, title='ID do Curso', desc
 
 
 @app.post('/cursos', status_code=status.HTTP_201_CREATED, tags=['Adicionando um Curso no banco de dados'])
-async def add_curso(curso: CursosModel):
+async def add_curso(curso: CursosModel, db: Any = Depends(fake_db)):
     try:
         next_id = len(cursos) + 1
         cursos[next_id] = curso
@@ -60,7 +72,8 @@ async def add_curso(curso: CursosModel):
 
 @app.put('/cursos/{curso_id}', status_code=status.HTTP_202_ACCEPTED, tags=['Atualizando curso de forma individual'])
 async def update_curso(curso: CursosModel, curso_id: int = Path(default=None, title='ID do curso',
-                                                                description='Adicione o Id que deseja atualizar')):
+                       description='Adicione o Id que deseja atualizar'),
+                       db: Any = Depends(fake_db)):
     if curso_id in cursos:
         del curso.id
         cursos[curso_id] = curso
@@ -73,7 +86,8 @@ async def update_curso(curso: CursosModel, curso_id: int = Path(default=None, ti
 
 @app.delete('/cursos/{curso_id}', status_code=status.HTTP_205_RESET_CONTENT, tags=['Deletando curso de forma individual'])
 async def delete_curso(curso_id: int = Path(default=None, title='ID do curso que deseja deletar',
-                                            description='Uso o ID para deletar o curso')):
+                       description='Uso o ID para deletar o curso'),
+                       db: Any = Depends(fake_db)):
     """
     Usar essa rota apenas no caso de querer deletar um curso\n
     :param curso_id: ID do curso\n
@@ -88,25 +102,26 @@ async def delete_curso(curso_id: int = Path(default=None, title='ID do curso que
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Não foi encontrado arquivo para se deletar')
 
 
-@app.get('/calculadora')
-async def calcular(a: int, b: int, c: Optional[int] = 0):
+@app.get('/calculadora', tags=['Programando a Calculadora'])
+async def calcular(a: int, b: int, c: Optional[int] = 0, db: Any = Depends(fake_db)):
 
     soma = a + b + c
 
     return {'mensagem': soma}
 
 
-@app.get('/calculadora2')
-async def calcular2(a: int = Query(default=None, ge=0), b: int = Query(default=None, ge=0), c: Optional[int] = 0):
+@app.get('/calculadora2', tags=['Programando a Calculadora'])
+async def calcular2(a: int = Query(default=None, ge=0), b: int = Query(default=None, ge=0), c: Optional[int] = 0,
+                    db: Any = Depends(fake_db)):
 
     soma = a + b + c
 
     return {'mensagem': soma}
 
 
-@app.get('/calculadora3')
+@app.get('/calculadora3', tags=['Programando a Calculadora'])
 async def calculadora3(a: int = Query(default=None, ge=0), b: int = Query(default=None, ge=0),
-                       x_geek: str = Header(default=None), c: Optional[int] = 0):
+                       x_geek: str = Header(default=None), c: Optional[int] = 0, db: Any = Depends(fake_db)):
 
     soma = a + b + c
     print(f'Testando header: {x_geek}')
