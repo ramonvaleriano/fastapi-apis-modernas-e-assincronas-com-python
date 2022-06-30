b13d3c1f8c19690d7aa0de3d6d2f0105df3388fe
ramon.valeriano <ramon.valeriano@netrin.com.br>
2022-06-30 19:55:44 -0300

    [0.0.1]-[20220630]_6-criando-validador-para-as-models

--- Aulas/Aula002/models.py
+++ Aulas/Aula002/models.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class CursosModel(BaseModel):
@@ -8,3 +8,34 @@ class CursosModel(BaseModel):
     titulo: str
     aulas: int
     horas: int
+
+    @validator('titulo')
+    def validar_titulo(cls, value: str):
+        palavras = value.split(' ')
+        if len(palavras) < 3:
+            raise ValueError('O título deve ver pelo menos 3 palavras')
+
+        if value.islower():
+
+            raise ValueError('O titulo não pode ser todo minusculo')
+
+        return value
+
+    @validator('aulas')
+    def validar_aulas(cls, value: int):
+        if value <= 0:
+            raise ValueError('Por favor, apenas número positivos')
+        if value < 12:
+            raise ValueError('O número do aulas tem que ser maior que 12')
+
+        return value
+
+    @validator('horas')
+    def validar_hotas(cls, value: int):
+        if value <= 0:
+            raise ValueError('Por favor, apenas número positivos')
+        if value < 10:
+            raise ValueError('O número do horas tem que ser mais que 10 horas')
+
+        return value
+    
